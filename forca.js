35c7c3fb6064497aee517847b0c0b8728c7556a2
Elaine-Pereira <elainepereirasantosss@gmail.com>
2022-06-02 23:31:06 -0300

    Novas alterações no js

--- forca.js
+++ forca.js
@@ -1,4 +1,28 @@
+// deseha tabuleiro
+function tabuleiro (){
+    var canvas = document.getElementById('canvas');
+    if(canvas.getContext){
+        var pincel = canvas.getContext('2d');
+    }
+}
+//função gera galavra
+function palavrasAleatorias (palavras){
+    var palavras= ['amor', 'felicidade', 'paz', 'gratidao','sucesso','inteligencia','musica'];
+    var busca = palavras[Math.ceil(Math.random() * (palavras.length - 1))];
+}
+console.log(palavrasAleatorias())
 
-<script>
-fillRect(0,0, 600, 800);
-    </script>
+
+
+//check se a palavra cabe no espaço
+function check(espaço, palavras){
+    if(espaço.length !== palavras.length){
+        return false;
+    } for (let i = 0; i < espaço.length; i++){
+    if(espaço[i] !== '-' && espaço[i] !== palavras[i]){
+        return false;
+    } //se não couber  
+    }
+    return true;  //se couber no espaço
+}
+
